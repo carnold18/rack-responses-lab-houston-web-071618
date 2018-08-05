4f1ed10dd17782be711bf554f0dbe0fb33bdc11a
Christopher Arnold <chrisarnold2010@gmail.com>
2018-08-05 16:19:27 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,8 +1,8 @@
 class Application
   def call(env)
     time = Time.now
-    resp.write "#{time}\n"
-    if time < 
+    resp.write "#{time.hour}\n"
+    if time.hour < 12
     	resp.write "Good Morning!"
     else
     	resp.write "Good Afternoon!"
